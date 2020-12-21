616683ef0f9533e17ce6d3fa24692f94f564311a
olexii <omedvedew@ukr.net>
2020-12-21 11:56:05 +0200

    added modal window

---
+++ app.js
@@ -0,0 +1,24 @@
+let modal = document.querySelector(".modal");
+
+let closeBtn = document.querySelector(".close-btn");
+
+function closeModal() {
+    modal.classList.remove("show");
+    modal.classList.add("hide");
+};
+
+function showModal() {
+    modal.classList.remove("hide");
+    modal.classList.add("show");
+};
+
+closeBtn.addEventListener("click", closeModal);
+
+let body = document.getElementById("home-anchor");
+
+window.addEventListener("scroll", function() {
+    console.log(pageYOffset);
+    if (pageYOffset >= body.offsetHeight/2 && pageYOffset <= body.offsetHeight/2 + 50) {
+        showModal();
+    };
+});
