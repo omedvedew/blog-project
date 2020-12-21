246781dd7c103d2a8de0d7824a8594efecf68d6f
olexii <omedvedew@ukr.net>
2020-12-21 21:14:01 +0200

    added mobile-menu

--- app.js
+++ app.js
@@ -35,3 +35,16 @@ console.log(sendMessageModal);
 sendMessageBtn.addEventListener("click", function() {
    sendMessageModal.classList.toggle("show");
 });
+
+// mob menu
+let mobileMenu = document.querySelector(".nav-mobile-menu");
+let mainMenu = document.querySelector(".navigation");
+
+mobileMenu.addEventListener("click", function() {
+    mobileMenu.classList.toggle("active-menu");
+    if (mobileMenu.classList.contains("active-menu")) {
+        mainMenu.classList.add("active-menu");
+    } else {
+        mainMenu.classList.remove("active-menu");
+    };
+});
